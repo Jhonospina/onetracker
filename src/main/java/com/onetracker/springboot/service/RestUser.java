9db2d2bc378192f46a5ad19e2aeb7dc6e1efcef0
Jhonospina <jhonospina2711@gmail.com>
2019-11-14 23:02:05 +0000

    prueba

--- src/main/java/com/onetracker/springboot/service/RestUser.java
+++ src/main/java/com/onetracker/springboot/service/RestUser.java
@@ -1,5 +1,7 @@
 package com.onetracker.springboot.service;
 
+import java.util.logging.Logger;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,65 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.onetracker.springboot.vo.VOUser;
 
 @RestController
-@RequestMapping(path = "/servicesREST/JR")
+@RequestMapping(path = "/api")
 public class RestUser {
+	
+	private static final Logger LOGGER = Logger.getLogger(RestUser.class.getName());
 
 	@RequestMapping(
 			method = RequestMethod.POST, 
-			path = "/validateUser", 
+			path = "/track", 
 			consumes = "application/json", 
 			produces = "application/json")
 	
-	public @ResponseBody VOUser validateUser(@RequestBody VOUser user) throws Exception {
-		
-		
-		
-		
-		VOUser response = new VOUser();
-		response.setFind(false);
-		
-		String lat = user.getlat();
-		String lon = user.getlon();
-		String speed = user.getspeed();
-		String time = user.gettime();
-		String http = user.gethttp();
-		String pwr = user.getpwr();
-		String door = user.getdoor();
-		String acc = user.getacc();
-		
-		System.out.println("latitud: " + lat);
-		System.out.println("longitud: " + lon);
-		System.out.println("speed: " + speed);
-		System.out.println("Hora: " + time);
-		System.out.println("http: " + http);
-		System.out.println("Power: " + pwr);
-		System.out.println("Puerta: " + door);
-		System.out.println("Energia: " + acc);
-		
-		try {
-			if (user.getlat().equals(lat) 
-					&& user.getlon().equals(lon)
-					&& user.getspeed().equals(speed)
-					&& user.gettime().equals(time)
-					&& user.gethttp().equals(http)
-					&& user.getpwr().equals(pwr)
-					&& user.getdoor().equals(door)
-					&& user.getacc().equals(acc)
-					) {
-				response.setlat(lat);
-				response.setlon(lon);
-				response.setspeed(speed);
-				response.settime(time);
-				response.sethttp(http);
-				response.setpwr(pwr);
-				response.setdoor(door);
-				response.setacc(acc);
-				response.setFind(true);
-			}
-			return response;
-		} catch (Exception ex) {
-
-			return response;
-		}
+	public @ResponseBody String validateUser(@RequestBody Object temp) throws Exception {
+		String cadena = (String) temp;
+		LOGGER.info("peticion hecha: "+cadena);
+		return "OK";
 	}
 }
